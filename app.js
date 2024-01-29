f9bc7873c5b2fa9d05c0c94faeea59557744312b
Nazar <n.konevych@campus.lmu.de>
2024-01-30 00:18:18 +0100

    integrate bootstrap

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 /**
  * Required External Modules
  */
-const express = require("express");       // server
-const path = require("path");
-const mongoose = require('mongoose');     // DB
+const express = require("express")       // server
+const path = require("path")
+const mongoose = require('mongoose')     // DB
 const fileupload = require("express-fileupload")
-const cookieParser = require('cookie-parser')
 const session = require('express-session')
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo')
 
 /**
  * App Variables
  */
-const app = express();
-const port = process.env.PORT || "8000";
-const mongoURI = "mongodb://localhost:27017";
+const app = express()
+const port = process.env.PORT || "8000"
+const mongoURI = "mongodb://localhost:27017"
+// const mongoURI = "mongodb://localhost:27017/kino_db"
 
 /**
  *  App Configuration
  */
 // pug config
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "src/views"))
+app.set("view engine", "pug")
 // expressjs config
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
 // file upload
 app.use(fileupload())
+// bootstrap
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
 
 // parse application/json
-app.use(express.json( { extended: false } ));
+app.use(express.json( { extended: false } ))
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded( { extended: false } ));
+app.use(express.urlencoded( { extended: false } ))
 // user authentication with secret as param
-// app.use(cookieParser('123'))
 app.use(session({
   secret: 'indiana jones is the best',
   resave: true,
@@ -50,21 +53,21 @@ app.use(session({
 /**
  * Routes Definitions
  */
-require('./src/routes/tools/defaults')(app);
-require('./src/routes/routes')(app);
-require('./src/routes/tools/errors')(app);
+require('./src/routes/tools/defaults')(app)
+require('./src/routes/routes')(app)
+require('./src/routes/tools/errors')(app)
 
 /**
  * Server Activation
  */
 const start = async () => {
     try {
-      await mongoose.connect(mongoURI);
-      app.listen(port, () => console.log(`Listening to requests on http://localhost:${port}`));
+      await mongoose.connect(mongoURI)
+      app.listen(port, () => console.log(`Listening to requests on http://localhost:${port}`))
     } catch (error) {
-      console.error(error);
-      process.exit(1);
+      console.error(error)
+      process.exit(1)
     }
-};
+}
 
-start();
+start()
